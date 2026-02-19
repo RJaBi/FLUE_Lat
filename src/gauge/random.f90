195829dae5a235372682106a12295434d587340d
RJaBi <ryan07bignell+github@gmail.com>
2026-02-19 11:34:01 +1030

    construct a SU3 matrix. det is correct

---
+++ src/gauge/random.f90
@@ -0,0 +1,42 @@
+module FLUE_SU3_random
+  use FLUE_constants, only: WP, WC
+  use FLUE_SU2_random, only: randomNumbers, constructSU2Matrix
+  use stdlib_intrinsics, only: stdlib_matmul
+  implicit none(external)
+  private
+
+  public :: constructSU3Matrix
+
+contains
+
+  pure function constructSU3Matrix(R,S,T) result(U)
+    complex(kind=WC), dimension(2,2), intent(in) :: R, S, T
+    complex(kind=WC), dimension(3,3) :: U
+    !! embed r,s,t into 3x3 matrices
+    complex(kind=WC), dimension(3,3) :: RE, SE, TE
+    !! in eqn 4.31 of Gattringer Lang
+    !! embed r
+    RE = (0.0_WP, 0.0_WP)
+    RE(1,1) = r(1,1)
+    RE(1,2) = r(1,2)
+    RE(2,1) = r(2,1)
+    RE(2,2) = r(2,2)
+    RE(3,3) = (1.0_WP, 0.0_WP)
+    !! embed s
+    SE = (0.0_WP, 0.0_WP)
+    SE(1,1) = s(1,1)
+    SE(1,3) = s(1,2)
+    SE(3,1) = s(2,1)
+    SE(3,3) = s(2,2)
+    SE(2,2) = (1.0_WP, 0.0_WP)
+    !! embed t
+    TE = (0.0_WP, 0.0_WP)
+    TE(1,1) = (1.0_WP, 0.0_WP)
+    TE(2,2) = t(1,1)
+    TE(2,3) = t(1,2)
+    TE(3,2) = t(2,1)
+    TE(3,3) = t(2,2)
+    U = stdlib_matmul(RE,SE,TE)
+  end function constructSU3Matrix
+
+end module FLUE_SU3_random
--- test/check.f90
+++ test/check.f90
@@ -1,6 +1,7 @@
 program check
   use FLUE, only: WP, WC, &
-       constructSU2Matrix, randomNumbers
+       constructSU2Matrix, randomNumbers,  &
+       constructSU3Matrix
   use stdlib_random, only: random_seed
   use stdlib_linalg, only: det
   use stdlib_math, only: is_close
@@ -12,6 +13,11 @@ program check
   real(kind=WP), dimension(4) :: SU2_r
   complex(kind=WC), dimension(2,2) :: SU2_U
   real(kind=WP) :: SU2_det
+  ! SU3 variables
+  real(kind=WP), dimension(4,3) :: SU3_r
+  complex(kind=WC), dimension(2,2) :: SU3_SU2_r, SU3_SU2_s, SU3_SU2_t
+  complex(kind=WC), dimension(3,3) :: SU3_U
+  real(kind=WP) :: SU3_det
   ! Do the seed
   call random_seed(put=seedPut, get=seedGet)
   ! now do some tests
@@ -24,5 +30,25 @@ program check
      stop
   end if
   !print *, "Put some tests in here!"
-  write(*,*) 'check done'
+  write(*,*) 'SU2 checks done'
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  !! Now do some SU3 checks
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  ! Get random numbers
+  SU3_r(:, 1) = randomNumbers()
+  SU3_r(:, 2) = randomNumbers()
+  SU3_r(:, 3) = randomNumbers()
+  ! Get some SU2 matrices
+  SU3_SU2_r = constructSU2Matrix(SU3_r(:, 1))
+  SU3_SU2_s = constructSU2Matrix(SU3_r(:, 2))
+  SU3_SU2_t = constructSU2Matrix(SU3_r(:, 3))
+  ! Do the SU3 matrix
+  SU3_U = constructSU3Matrix(SU3_SU2_r, SU3_SU2_s, SU3_SU2_t)
+  SU3_det = det(SU3_U)
+  if (.not. is_close(SU3_det, 1.0_WP)) then
+     write(*,*) 'SU3_det', SU3_det, 'not close to 1.0'
+     write(*,*) 'SU3_U is ', SU3_U
+     stop
+  end if
+  write(*,*) 'SU3 checks done'
 end program check
