c7aa0ba66d524a5c59c7c35a46efd385db956e42
RJaBi <ryan07bignell+github@gmail.com>
2026-06-11 19:50:02 +0930

    Feature: Add NRQ2CD reader

--- src/FLUE.f90
+++ src/FLUE.f90
@@ -12,7 +12,7 @@ MODULE FLUE
    USE FLUE_SU2_CSSM, ONLY: writeGaugeField_SU2_CSSM
    USE FLUE_SU2_heatbath, ONLY: SU2_updateLinks, constructXMatrix
    USE FLUE_SU2_HKLS, ONLY: ReadGaugeField_HKLS, WriteGaugeField_HKLS
-   USE FLUE_SU2_NRQ2CD, ONLY: writeGaugeField_NRQ2CD
+   USE FLUE_SU2_NRQ2CD, ONLY: writeGaugeField_NRQ2CD, readGaugeField_NRQ2CD
    USE FLUE_SU2_random, ONLY: constructSU2Matrix, randomNumbers
    USE FLUE_SU2_wloops, ONLY: SU2_genPlaquette, SU2_genericPath
    USE FLUE_SU3_random, ONLY: constructSU3Matrix
--- src/gaugeIO_SU2/NRQ2CD.f90
+++ src/gaugeIO_SU2/NRQ2CD.f90
@@ -5,6 +5,7 @@ MODULE FLUE_SU2_NRQ2CD
   IMPLICIT NONE(TYPE, EXTERNAL)
   PRIVATE
   PUBLIC :: writeGaugeField_NRQ2CD
+  PUBLIC :: readGaugeField_NRQ2CD
 CONTAINS
   SUBROUTINE writeGaugeField_NRQ2CD(filename, NX, NY, NZ, NT, U)
     CHARACTER(len=*), INTENT(IN) :: filename
@@ -17,6 +18,7 @@ CONTAINS
    COMPLEX(kind=WC), DIMENSION(:,:,:,:,:), ALLOCATABLE :: s
    ip5d2 = int(NX*NY*NZ*NT*0.5)
    ALLOCATE(s(ip5d2, 2, 2, 4, 0:1))
+   WRITE(*,*) 'a', U(:,:,2,1,1,3,1)
    !------------------------------------------------------------
    ! Rearrangement
    !------------------------------------------------------------
@@ -43,6 +45,10 @@ CONTAINS
          END DO
       END DO
    END DO
+   ! Shift the mu-ordering so that xyzt
+   s = cshift(s, 1, dim=4)
+   WRITE(*,*) 'b25o', s(25,:,:,1,0)
+   WRITE(*,*) 'b25e', s(25,:,:,1,1)
    !------------------------------------------------------------
    ! Write to disk (raw binary, stream I/O)
    !------------------------------------------------------------
@@ -51,7 +57,53 @@ CONTAINS
    WRITE(infl) s
    CLOSE(infl)
    DEALLOCATE(s)
-
  END SUBROUTINE writeGaugeField_NRQ2CD
+
+
+ SUBROUTINE readGaugeField_NRQ2CD(filename, NX, NY, NZ, NT, U)
+   CHARACTER(len=*), INTENT(IN) :: filename
+   INTEGER, INTENT(IN) :: NX, NY, NZ, NT
+   COMPLEX(kind=WC), DIMENSION(2,2,4,NT,NX,NY,NZ), INTENT(OUT) :: U
+   INTEGER :: ix, iy, iz, it, mu
+   INTEGER :: ix2, ip, par
+   INTEGER :: infl, ip5d2
+   COMPLEX(kind=WC), DIMENSION(:,:,:,:,:), ALLOCATABLE :: s
+   ! Optional but sensible: the mapping assumes NX is even
+   IF (MOD(NX, 2) /= 0) THEN
+      WRITE(*,*) "readGaugeField_NRQ2CD: NX must be even.", NX
+      STOP
+   END IF
+   ip5d2 = NX*NY*NZ*NT/2
+   ALLOCATE(s(ip5d2, 2, 2, 4, 0:1))
+   !------------------------------------------------------------
+   ! Read raw binary block
+   !------------------------------------------------------------
+   OPEN(newunit=infl, file=filename, form="unformatted", &
+        access="stream", status="old", action="read")
+   READ(infl) s
+   CLOSE(infl)
+   !------------------------------------------------------------
+   ! Inverse rearrangement
+   !------------------------------------------------------------
+   DO it = 1, NT
+      DO iz = 1, NZ
+         DO iy = 1, NY
+            DO ix = 1, NX
+               par = MOD(ix + iy + iz + it, 2)
+               ix2 = (ix - 1) / 2
+               ip  = ((((it-1)*NZ + (iz-1))*NY + (iy-1)) * (NX/2)) + ix2 + 1
+               DO mu = 1, 4
+                  U(:,:,mu,it,ix,iy,iz) = s(ip,:,:,mu,par)
+               END DO
+            END DO
+         END DO
+      END DO
+   END DO
+   ! Shift the mu-index so that txyz
+   U = cshift(U, -1, dim=3)
+   DEALLOCATE(s)
+ END SUBROUTINE readGaugeField_NRQ2CD
+
+
 
 END MODULE FLUE_SU2_NRQ2CD
